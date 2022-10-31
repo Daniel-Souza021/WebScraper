3704334742bfd36953c0cbc6853e47f8a1e3b497
Daniel Souza <daniel.souza@unoesc.edu.br>
2022-10-30 23:36:50 -0300

    #017 - Melhorias filtros sessao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  before_action :filtros_sessao
+  before_action :filtros_sessao, except: :historico
 
   def filtros_sessao
     if session
       session[:filtros] ||= {}
-      session[:filtros][:"#{params[:controller]}##{params[:action]}"] = params["produto"]["descricao"] if params.has_key? "produto"
+      session[:filtros][:"#{params[:controller]}##{params[:action]}"] = params.dig(:produto,:descricao).to_s
     end
 
   end
