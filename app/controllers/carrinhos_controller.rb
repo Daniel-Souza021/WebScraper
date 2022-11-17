5cc766820f2afb7f795817624572a4b4585c40c8
Daniel Souza <daniel.souza@unoesc.edu.br>
2022-11-16 23:32:07 -0300

    #027 - melhorias gerais e refatoração consulta

--- app/controllers/carrinhos_controller.rb
+++ app/controllers/carrinhos_controller.rb
@@ -1,19 +1,7 @@
 class CarrinhosController < ApplicationController
-  def index
-
-  end
+  def index;end
 
   def busca_produtos
-    if params['produtos'].present?
-      @produtos = Produto.valido.select(
-        "DISTINCT ON(descricao, codigo_mercado)
-      descricao,
-      preco,
-      codigo_mercado,
-      link_imagem,
-      link_endereco").where(descricao: JSON.parse(params['produtos'])).where(created_at: Date.today.all_day)
-    else
-      @produtos = []
-    end
+    @produtos = params['produtos'].present? ? Produto.busca_produtos_historicos({ historico: JSON.parse(params['produtos']) }) : []
   end
 end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -13,17 +13,105 @@ class Produto < ApplicationRecord
     end
 
     def busca_produtos params = {}
-      sql_produtos = Produto.valido.
-        where("produtos.created_at >= (?)", Time.now.beginning_of_day).
-        where("EXISTS(SELECT TRUE FROM produtos as prod WHERE prod.descricao = produtos.descricao AND prod.codigo_mercado = 2
-               AND prod.link_imagem LIKE '%_mini.jpg%' AND prod.created_at >= (?))", Time.now.beginning_of_day).
-        where("EXISTS(SELECT TRUE FROM produtos as prod WHERE prod.descricao = produtos.descricao AND prod.codigo_mercado = 1
-               AND prod.link_imagem LIKE '%_mini.jpg%' AND prod.created_at >= (?))", Time.now.beginning_of_day).
-        where("preco = (SELECT MIN(prod.preco) FROM produtos AS prod WHERE prod.descricao = produtos.descricao)").order(:descricao).to_sql
+      produtos = connection.execute("
+      DROP table IF EXISTS mercado1;
+      DROP table IF EXISTS mercado2;
+      DROP table IF EXISTS produtos_final;
 
-      produtos = Produto.select("DISTINCT ON(descricao) *").from("(#{sql_produtos }) AS products").order(:descricao)
+      CREATE TEMP TABLE mercado1 AS
+      select
+      distinct on
+      (descricao) descricao,
+        created_at, codigo_mercado, preco, id
+      from
+      produtos
+      where
+      codigo_mercado = 1 and link_imagem LIKE '%_mini.jpg%'
+      order by
+      descricao,
+        created_at desc, codigo_mercado, preco desc;
+
+      CREATE TEMP TABLE mercado2 AS
+      select
+      distinct on
+      (descricao) descricao,
+        created_at,
+        codigo_mercado, preco, id
+      from
+      produtos
+      where
+      codigo_mercado = 2 and link_imagem LIKE '%_mini.jpg%'
+      order by
+      descricao,
+        created_at desc, codigo_mercado, preco desc;
+
+      CREATE TEMP TABLE produtos_final AS
+      (
+        (select mercado1.* from mercado1
+        join mercado2 on mercado2.descricao = mercado1.descricao)
+        union all
+        (select mercado2.* from mercado2
+        join mercado1 on mercado1.descricao = mercado2.descricao)
+      );
+
+      select distinct on (descricao) preco, id
+      from produtos_final
+      order by
+      descricao, preco asc;")
+
+      produtos = Produto.where(id: produtos.values.map(&:last)).order(:descricao)
       produtos.where!("descricao ilike '%#{params[:filtros][:descricao].to_s.downcase}%'") if params[:filtros].present?
+      produtos.where!(descricao: params[:historico]) if params[:historico].present?
       produtos.paginate(page: params[:page], per_page: 18)
+    end
+
+    def busca_produtos_historicos params = {}
+      produtos = connection.execute("
+      DROP table IF EXISTS mercado1;
+      DROP table IF EXISTS mercado2;
+      DROP table IF EXISTS produtos_final;
+
+      CREATE TEMP TABLE mercado1 AS
+      select
+      distinct on
+      (descricao) descricao,
+        created_at, codigo_mercado, preco, id
+      from
+      produtos
+      where
+      codigo_mercado = 1 and link_imagem LIKE '%_mini.jpg%'
+      order by
+      descricao,
+        created_at desc, codigo_mercado, preco desc;
+
+      CREATE TEMP TABLE mercado2 AS
+      select
+      distinct on
+      (descricao) descricao,
+        created_at,
+        codigo_mercado, preco, id
+      from
+      produtos
+      where
+      codigo_mercado = 2 and link_imagem LIKE '%_mini.jpg%'
+      order by
+      descricao,
+        created_at desc, codigo_mercado, preco desc;
+
+      CREATE TEMP TABLE produtos_final AS
+      (
+        (select mercado1.* from mercado1
+        join mercado2 on mercado2.descricao = mercado1.descricao)
+        union all
+        (select mercado2.* from mercado2
+        join mercado1 on mercado1.descricao = mercado2.descricao)
+      );
+
+      select id
+      from produtos_final;")
+
+      produtos = Produto.where(id: produtos.values.map(&:last)).order(:descricao)
+      produtos.where!(descricao: params[:historico]) if params[:historico].present?
     end
   end
 end
